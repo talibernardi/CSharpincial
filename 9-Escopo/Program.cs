1393fab3c69bcbafde6b30fa8a6a1db74a2a93fc
Talita Bernardino Neves Silva <talita.silva@db1.com.br>
2021-09-20 17:51:31 -0300

    nona aula

---
+++ 9-Escopo/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _9_Escopo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando Projeto 9 - Escopo");
+            int idadeJoao = 18;
+            bool acompanhado = true;
+            string mensagemAdicional;
+            
+
+
+            if (acompanhado == true)
+            {
+                string mensagemAdicional = "João está acompanhado!";
+                Console.WriteLine(mensagemAdicional);
+            }
+            else
+            {
+                mensagemAdicional = "João não está acompanhado";
+            }
+            
+           
+
+            if (idadeJoao >= 18 || acompanhado == true)
+            {
+                Console.WriteLine("Pode entrar.");
+
+            }
+            else
+            {
+                Console.WriteLine("Não pode entrar");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
