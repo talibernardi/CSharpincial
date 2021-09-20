477a87f594c9ef2066a1b2b0b5b016938f48ce03
Talita Bernardino Neves Silva <talita.silva@db1.com.br>
2021-09-20 17:47:03 -0300

    quarta aula

---
+++ 4-ConversoesEOutrosTiposNumericos/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4_ConversoesEOutrosTiposNumericos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando o Projeto 4");
+            double salario = 1200.50;
+
+            //o int é um tipo de variavel que suporta valores de até 32 bits
+            int salarioEmInteiro = (int)salario;
+            
+
+            Console.WriteLine(salarioEmInteiro);
+            
+
+            //o long é um variavel de 64 bits
+            long idade = 130000000000000;
+            Console.WriteLine(idade);
+
+            //o short é um tipo de variavel de 16 bits
+            short quantidadeProdutos = 15000;
+            Console.WriteLine(quantidadeProdutos);
+
+            float altura = 1.80f;
+            Console.WriteLine(altura);
+            
+
+            Console.ReadLine();
+        }
+    }
+}
