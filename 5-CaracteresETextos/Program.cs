9c358a26fbf3bd086be23d2fa72639ff455a165d
Talita Bernardino Neves Silva <talita.silva@db1.com.br>
2021-09-20 17:47:48 -0300

    quinta aula

---
+++ 5-CaracteresETextos/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5_CaracteresETextos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando Projeto 5 - Caracteres E Textos");
+
+            //character
+            char primeiraLetra = ' ';
+            Console.WriteLine(primeiraLetra);
+
+            primeiraLetra = (char)65;
+            Console.WriteLine(primeiraLetra);
+
+            primeiraLetra = (char)(primeiraLetra + 1);
+            Console.WriteLine(primeiraLetra);
+
+            string titulo = "Alura Cursos de Tecnologia " + 2020;
+            string CursosProgramacao = 
+@"- .NET +
+- Java +
+- JavaScript";
+
+
+            Console.WriteLine(titulo);
+
+            string vazia = " ";
+
+
+            Console.WriteLine(CursosProgramacao);
+
+            
+
+            Console.ReadLine();
+        }
+    }
+}
+ 
